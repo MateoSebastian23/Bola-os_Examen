b4c0cc2bb1bb45fe1f1f13282d3f9ab8ad1559e3
MateoSebastian23 <msbm919@gmail.com>
2025-01-27 19:14:38 -0500

    Delete CODIGO/tratamiento-service/tratamiento-service directory

--- CODIGO/tratamiento-service/tratamiento-service/src/main/java/com/example/tratamiento_service/repository/TratamientoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tratamiento_service.repository;
-
-import com.example.tratamiento_service.entity.Tratamiento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TratamientoRepository extends JpaRepository<Tratamiento, Long> {
-}
