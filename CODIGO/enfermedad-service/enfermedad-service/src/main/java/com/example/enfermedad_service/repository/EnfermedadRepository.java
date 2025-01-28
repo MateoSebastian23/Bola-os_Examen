57c47ea85e8fea8c178b82bf5e60bd78bb8cfb3a
MateoSebastian23 <msbm919@gmail.com>
2025-01-27 19:14:28 -0500

    Delete CODIGO/enfermedad-service/enfermedad-service directory

--- CODIGO/enfermedad-service/enfermedad-service/src/main/java/com/example/enfermedad_service/repository/EnfermedadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.enfermedad_service.repository;
-
-import com.example.enfermedad_service.entity.Enfermedad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnfermedadRepository extends JpaRepository<Enfermedad, Long> {
-}
